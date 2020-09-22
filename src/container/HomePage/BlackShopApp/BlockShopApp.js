c5fb4a60e90f472526bb8bc4f03a4d7a05a5834e
unknown <daniel.dallakyan@gmail.com>
2020-09-22 16:53:13 +0400

    BlockShopApp Is Ready

---
+++ src/container/HomePage/BlackShopApp/BlockShopApp.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import{FaStar} from "react-icons/fa";
+import './BlockShopApp.scss'
+
+function BlockShopApp(){
+    return(
+        <div className="BlockShopApp">
+            
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+            <div>
+              <img src=""/>
+               <p className="title">Cruise Dual Analog</p>
+               <p className="price">$250.00</p>
+               <ul>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+                  <li><FaStar className="star"/></li>
+               </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default BlockShopApp;
