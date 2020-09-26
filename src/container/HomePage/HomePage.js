765af24d8c7d4d4daaa2912833edb659095e9833
unknown <daniel.dallakyan@gmail.com>
2020-09-26 20:05:49 +0400

    .

--- src/container/HomePage/HomePage.js
+++ src/container/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from '../../component/NavBar/NavBar';
 import Category from './Category/Category';
-import ShopList from './ShopList/ShopList';
+import ShopList from './../HomePage/components/ShopList/ShopList';
 function HomePage (){  
    return(    
     <div className="homepage1">
